a7ed886ed21b41a1e1bcd2c1e8bb1e4ca8999d3a
Justin Taylor <taylo5jd@dukes.jmu.edu>
2022-11-14 10:27:01 -0500

    fixed join request/ edit user/ edit project

--- Pages/Project/EditProject.cshtml.cs
+++ Pages/Project/EditProject.cshtml.cs
@@ -11,61 +11,63 @@ namespace lab_1_part_3.Pages.Project
         public ProjectProfile ProjectToUpdate { get; set; }
         [BindProperty]
         public List<UserProfile> UserList { get; set; }
+   
+
         public EditProjectModel()
         {
             ProjectToUpdate = new ProjectProfile();
             UserList = new List<UserProfile>();
+
         }
-
-        public void OnGet(int ProjectID)
-        {
-            if (HttpContext.Session.GetString("username") == null)
+            public void OnGet(int ProjectID)
             {
-                RedirectToPage("/DBLogin");
-            }
-            SqlDataReader singleProject = DBProjectClass.SingleProjectReader(ProjectID);
-
-            while (singleProject.Read())
-            {
-                ProjectToUpdate.ProjectID = ProjectID;
-                ProjectToUpdate.ProfileID = Int32.Parse(singleProject["ProfileID"].ToString());
-                ProjectToUpdate.ProjectDescription = singleProject["Project_Description"].ToString();
-                ProjectToUpdate.ProjectType = singleProject["Project_Type"].ToString();
-                ProjectToUpdate.ProjectName = singleProject["Project_Name"].ToString();
-                ProjectToUpdate.ProjectOwnerEmail = singleProject["Project_Owner_Email"].ToString();
-
-
-            }
-            SqlDataReader userReader = DBUserClass.MultipleUserReader();
-
-            while (userReader.Read())
-            {
-                UserList.Add(new UserProfile
+                if (HttpContext.Session.GetString("username") == null)
                 {
-                    ProfileID = Int32.Parse(userReader["ProfileID"].ToString()),
-                    Username = userReader["Username"].ToString(),
-                    FirstName = userReader["FirstName"].ToString(),
-                    LastName = userReader["LastName"].ToString(),
-                    Email = userReader["Email"].ToString(),
-                    UserType = userReader["UserType"].ToString(),
-                    PhoneNumber = userReader["PhoneNumber"].ToString(),
-                    ProfessionalEmail = userReader["Professional_Email"].ToString(),
-                    ProfessionalCompany = userReader["Professional_Company"].ToString(),
-                    FacultyAssociation = userReader["Faculty_Association"].ToString(),
+                    RedirectToPage("/DBLogin");
+                }
+                SqlDataReader singleProject = DBProjectClass.SingleProjectReader(ProjectID);
 
-                });
+                while (singleProject.Read())
+                {
+                    ProjectToUpdate.ProjectID = ProjectID;
+                    ProjectToUpdate.ProfileID = Int32.Parse(singleProject["ProfileID"].ToString());
+                    ProjectToUpdate.ProjectDescription = singleProject["Project_Description"].ToString();
+                    ProjectToUpdate.ProjectType = singleProject["Project_Type"].ToString();
+                    ProjectToUpdate.ProjectName = singleProject["Project_Name"].ToString();
+                    ProjectToUpdate.ProjectOwnerEmail = singleProject["Project_Owner_Email"].ToString();
 
-               
+
+                }
+                SqlDataReader userReader = DBUserClass.MultipleUserReader();
+
+                while (userReader.Read())
+                {
+                    UserList.Add(new UserProfile
+                    {
+                        ProfileID = Int32.Parse(userReader["ProfileID"].ToString()),
+                        Username = userReader["Username"].ToString(),
+                        FirstName = userReader["FirstName"].ToString(),
+                        LastName = userReader["LastName"].ToString(),
+                        Email = userReader["Email"].ToString(),
+                        UserType = userReader["UserType"].ToString(),
+                        PhoneNumber = userReader["PhoneNumber"].ToString(),
+                        ProfessionalEmail = userReader["Professional_Email"].ToString(),
+                        ProfessionalCompany = userReader["Professional_Company"].ToString(),
+                        FacultyAssociation = userReader["Faculty_Association"].ToString(),
+
+                    });
+
+
+                }
+                singleProject.Close();
+                userReader.Close();
             }
-            singleProject.Close();
-            userReader.Close();
-        }
 
-        public IActionResult OnPost()
-        {
-            DBProjectClass.UpdateProject(ProjectToUpdate);
+            public IActionResult OnPost()
+            {
+                DBProjectClass.UpdateProject(ProjectToUpdate);
 
-            return RedirectToPage("Index");
+                return RedirectToPage("Index");
+            }
         }
-    }
-}
+    } 
