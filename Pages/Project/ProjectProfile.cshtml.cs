a9fd94f83b64913706624b134906d9e5ec28e682
Justin Taylor <taylo5jd@dukes.jmu.edu>
2022-11-30 09:44:51 -0500

    added rec users to project profile but need to fix display

--- Pages/Project/ProjectProfile.cshtml.cs
+++ Pages/Project/ProjectProfile.cshtml.cs
@@ -11,10 +11,15 @@ namespace lab_1_part_3.Pages.Project
         public List<ProjectProfile> ProjectList { get; set; }
         [BindProperty]
         public List<IFormFile> files { get; set; }
-
+        public List<UserProfile> UserList { get; set; }
+        public List<UserProfile> UserList2 { get; set; }
+        [BindProperty]
+        public int projID { get; set; }
         public ProjectProfileModel()
         {
             ProjectList = new List<ProjectProfile>();
+            UserList = new List<UserProfile>();
+            UserList2 = new List<UserProfile>();
         }
 
         public IActionResult OnGet(int ProjectID)
@@ -45,6 +50,75 @@ namespace lab_1_part_3.Pages.Project
 
                 //projectReader.Close();
             }
+            projID = ProjectID;
+            SqlDataReader tempReader = DBClasses.DBDesiredSkills.UserReader(ProjectID);
+            while (tempReader.Read())
+            {
+                SqlDataReader userReader = DBUserClass.SingleUserReader(Int32.Parse(tempReader["ProfileID"].ToString()));
+
+
+
+                while (userReader.Read())
+                {
+                    UserList.Add(new UserProfile
+                    {
+                        Username = userReader["Username"].ToString(),
+                        ProfileID = Int32.Parse(userReader["ProfileID"].ToString()),
+                        FirstName = userReader["FirstName"].ToString(),
+                        LastName = userReader["LastName"].ToString(),
+                        Email = userReader["Email"].ToString(),
+                        UserType = userReader["UserType"].ToString(),
+                        PhoneNumber = userReader["PhoneNumber"].ToString(),
+                        ProfessionalEmail = userReader["Professional_Email"].ToString(),
+                        ProfessionalCompany = userReader["Professional_Company"].ToString(),
+                        FacultyAssociation = userReader["Faculty_Association"].ToString(),
+                        VideoIntroduction = userReader["Video_Introduction"].ToString(),
+                        LinkedIn = userReader["LinkedIn"].ToString()
+
+
+                    });
+
+
+                }
+
+
+
+                userReader.Close();
+
+
+            }
+
+            int test = 0;
+            bool flag = false;
+
+            for (int i = 0; i < UserList.Count; i++)
+            {
+                if (UserList2.Count == 0)
+                {
+                    UserList2.Add(UserList[i]);
+                    continue;
+                }
+
+                for (int k = 0; k < UserList2.Count; k++)
+                {
+                    if ((UserList[i].Username.Equals(UserList2[k].Username)))
+                    {
+                        flag = true;
+                        break;
+
+                    }
+
+                }
+                if (flag == false)
+                {
+                    UserList2.Add(UserList[i]);
+                }
+                else
+                {
+                    flag = false;
+                }
+            }
+
             return Page();
         }
     }
